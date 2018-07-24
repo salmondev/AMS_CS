642bb904be6b420dacdb3b86a23b578a7338a4b8
ARMY <panurut.army@hotmail.com>
2018-07-24 10:39:11 +0700

    Edit Home.php
    Edit home page

--- application/views/home.php
+++ application/views/home.php
@@ -9,6 +9,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <!-- Meta -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+     
+    <!-- Font Awesome's -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
 
     <!-- Favicon -->
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
@@ -27,16 +30,28 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.css">
     <link rel="stylesheet" href="fluid-gallery.css">
 
+<style>
+#block_container
+{
+    text-align:left;
+}
+#bloc1, #bloc2
+{
+    display:inline;
+}
+</style>
   </head>
 
   <body> 
     <div style="margin: 2%;">
-
-    <h1  class="display-4 text-primary col-sm-12">
-ItemTracker    <br>
-<div style="margin-top: 5%; margin-left: 5%; margin-right: 5x%;">
-<img class="img-fluid "src="<?php echo base_url('assets/img/earth1.jpg');?>">
+    <div id="block_container">
+    <!--<div id="bloc1"><i class="fas fa-code" style="font-size: 4em;"></i></div> -->
+    <div id="bloc2"><h1 class="display-4 text-primary"> ItemTracker <h1> </div>  
 </div>
+<br>
+<!--<div style="margin-top: 2%;">
+<img class="img-fluid "src="<?php echo base_url('assets/img/wide1.jpg');?>">
+</div>-->
     
     <br>
   
@@ -85,5 +100,8 @@ ItemTracker    <br>
     <!-- Theme Settings and Calls -->
     <script src="<?php echo base_url('assets/js/global.js');?>"></script>
     <!-- END JAVASCRIPTS -->
+
+    <!-- Font Awesome's-->
+    <script defer src="https://use.fontawesome.com/releases/v5.1.1/js/all.js" integrity="sha384-BtvRZcyfv4r0x/phJt9Y9HhnN5ur1Z+kZbKVgzVBAlQZX4jvAuImlIz+bG7TS00a" crossorigin="anonymous"></script>
   </body>
 </html>
