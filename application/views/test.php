9045ef8cf11a2002659a25d2570b5eed9cdd358a
ARMY <panurut.army@hotmail.com>
2018-07-26 13:27:03 +0700

    add symbol

--- application/views/test.php
+++ application/views/test.php
@@ -292,8 +292,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
         </a>
         <ul class="treeview-menu">
           <li><a href="<?php echo site_url('building_table/index/index');?>"><i class="fa fa-institution"></i>Building</a></li>
-          <li><a href="<?php echo site_url('floor_table/index/index');?>"><i class="fa fa-database"></i>Floor</a></li>
-          <li><a href="<?php echo site_url('item_table/index/index');?>"><i class="fa fa-cudes"></i>Item</a></li>
+          <li><a href="<?php echo site_url('floor_table/index/index');?>"><i class="fa fa-server"></i>Floor</a></li>
+          <li><a href="<?php echo site_url('item_table/index/index');?>"><i class="fa fa-cubes"></i>Item</a></li>
           <li><a href="<?php echo site_url('owner_table/index/index');?>"><i class="fa fa-address-card"></i>Owner</a></li>
           <li><a href="<?php echo site_url('room_table/index/index');?>"><i class="fa fa-database"></i>Room</a></li>
           <li><a href="<?php echo site_url('status_table/index/index');?>"><i class="fa fa-database"></i>Status</a></li>
