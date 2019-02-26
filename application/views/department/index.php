8846a79b518714c88fc46184cb25dcc28e12ed74
ARMY <panurut.army@hotmail.com>
2019-02-26 20:46:29 +0700

    import csv  DEPARTMENT table

--- application/views/department/index.php
+++ application/views/department/index.php
@@ -1,3 +1,104 @@
+<?php  
+$connect = mysqli_connect("localhost", "amsappne_nfcdb", "AMSnfcapp1", "amsappne_nfc");
+mysqli_set_charset($connect,'utf8');
+
+
+if(isset($_POST["submit"]))
+{
+ if($_FILES['file']['name'])
+ {
+  $filename = explode(".", $_FILES['file']['name']);
+  if($filename[1] == 'csv')
+  {
+	 $handle = fopen($_FILES['file']['tmp_name'], "r");
+	 
+	 //Finding the position of fieldnames from Row 1
+$fieldnames = fgetcsv($handle);
+$departmentidindex = array_search("DEPARTMENTID", $fieldnames);
+$departmentnameindex = array_search("DEPARTMENTNAME", $fieldnames);
+
+
+   while($data = fgetcsv($handle))
+   {
+		    
+		$count = count($data);
+		$wantedColumns = array($departmentidindex,$departmentnameindex);
+		for ($i=0; $i < $count ; $i++) { 
+			if (in_array($i,$wantedColumns))
+			{
+				//echo $content[$i]."\t";
+			
+			   /*
+				$item1 = mysqli_real_escape_string($connect, $data[0]);
+				$item2 = mysqli_real_escape_string($connect, $data[1]);
+				$item3 = mysqli_real_escape_string($connect, $data[2]);
+				$item4 = mysqli_real_escape_string($connect, $data[3]);
+				$item5 = mysqli_real_escape_string($connect, $data[4]);
+				$item6 = mysqli_real_escape_string($connect, $data[5]);*/
+
+				$item1 = mysqli_real_escape_string($connect, $data[$departmentidindex]);
+				$item2 = mysqli_real_escape_string($connect, $data[$departmentnameindex]);
+
+				//$myDate =  date("y/m/d",strtotime(str_replace('/','-',$item4)));
+				
+                $query = "INSERT into DEPARTMENT (DEPARTMENTID,DEPARTMENTNAME) values('$item1','$item2')";
+								
+								//$query2 = "UPDATE into DEPARTMENT (DAPARTMENTID,DEPARTMENTNAME) values('$item1','$item2'')";
+				
+				
+				mysqli_query($connect, $query);
+
+				//mysqli_query($connect, $query2);
+			}
+		}
+				
+   }
+   fclose($handle);
+   echo "<script>
+   alert('Import Done');
+   window.location.href='http://amsapp.net/index.php/department/index/index';
+			  
+   </script>";
+  }
+ }
+}
+?>
+
+<style>
+		@media screen {
+  #printSection {
+      display: none;
+  }
+}
+
+@media print {
+  body * {
+    visibility:hidden;
+  }
+  #printSection, #printSection * {
+    visibility:visible;
+  }
+  #printSection {
+    position:absolute;
+    left:0;
+    top:0;
+  }
+}
+
+.modal {
+background: rgba(0,0,0,0.5);
+}
+
+.show {
+background: rgba(0, 0, 0, 0.22);
+}
+
+.modal-backdrop.in {
+background: rgba(0, 0, 0, 0.5);
+}
+
+</style>
+
 <div class="row">
     <div class="col-md-12">
         <div class="box">
@@ -5,7 +106,10 @@
                 
             	<div class="box-title">
                     <a href="<?php echo site_url('department/add'); ?>" class="btn btn-success btn-lg">ADD DEPARTMENT</a> 
-                </div>
+					<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal1">
+						IMPORT CSV
+					</button>
+				</div>
             </div>
             <div class="box-body">
                 <table id="example2" class="table table-striped">
@@ -31,4 +135,36 @@
             </div>
         </div>
     </div>
+</div>
+
+<!-- The Modal -->
+<div class="modal" id="myModal1">
+	<div class="modal-dialog">
+		<div class="modal-content">
+
+			<!-- Modal Header -->
+			<div class="modal-header">
+				<h4 class="modal-title">Import CSV</h4>
+				<button type="button" class="close" data-dismiss="modal">&times;</button>
+			</div>
+
+			<!-- Modal body -->
+			<div class="modal-body">
+				<form method="post" enctype="multipart/form-data" accept-charset="utf-8">
+
+
+					<input type="file" name="file" />
+					<br />
+					<input type="submit" name="submit" value="Import" class="btn btn-info" />
+
+				</form>
+			</div>
+
+			<!-- Modal footer 
+			<div class="modal-footer">
+				<button type="button" class="btn btn-primary" id="btnPrint">Print</button>
+			</div>-->
+
+		</div>
+	</div>
 </div>
