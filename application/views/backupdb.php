1816b05e2b4c82d686977204db7c2a060e6b3181
ARMY <panurut.army@hotmail.com>
2019-02-26 12:56:43 +0700

    improve UI/UX

--- application/views/backupdb.php
+++ application/views/backupdb.php
@@ -1,9 +1,9 @@
 <div class="panel panel-primary">
 
 	<div class="panel-body">
-		<a href="<?php echo site_url('backupdb/backup_db') ?>" class="btn btn-primary btn_main_wd"><i class="fa fa-download fa-fw fa-6x"></i><br /><br />
+		<a href="<?php echo site_url('backupdb/backup_db') ?>" class="btn btn-primary btn_main_wd"><i class="fa fa-download fa-fw fa-5x"></i><br /><br />
 			สำรองฐานข้อมูล</a>
-		<a href="<?php echo site_url('backupdb/index2/restoredb') ?>" class="btn btn-success btn_main_wd"><i class="fa fa-upload fa-fw fa-6x"></i><br /><br />
+		<a href="<?php echo site_url('backupdb/index2/restoredb') ?>" class="btn btn-success btn_main_wd"><i class="fa fa-upload fa-fw fa-5x"></i><br /><br />
 			กู้คืนฐานข้อมูล</a>
 	</div>
 
